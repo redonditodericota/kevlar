c5dd82816a3abf12ee622238d4516eb283d6090d
redonditodericota <pablo_aven@hotmail.com>
2020-02-18 00:21:51 -0300

    limpieza

--- Kevlar Bot/index.js
+++ Kevlar Bot/index.js
@@ -1,80 +1,52 @@
+//REQUIRES
 const commando = require('discord.js-commando');
 const path = require('path');
 const config = require('./config.json');
-const bot = new commando.Client({
-    commandPrefix:(config.prefix),
-    owner: (config.ownerID),
-});
+const sqlite3 = require('sqlite3').verbose();
 
-bot.registry.registerDefaultTypes()
-    .registerGroups([
-        ['tradeo', 'Trade']
-    ])
-    .registerDefaultGroups()
-    .registerDefaultCommands()
-    .registerCommandsIn(path.join(__dirname, 'commands'));
 
-bot.once('ready', () => {
-     console.log('Logged in!');
-     bot.user.setActivity('kevlar');
-});
+//CORE
 
-/////////////////////////////////////////////////////////
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('database', 'user', 'password', {
-	host: 'localhost',
-	dialect: 'sqlite',
-	logging: false,
-	storage: 'database.sqlite',
-});
+const bot = initializeBot();
 
-const inventario = sequelize.define('inventario', {
-	name: {
-		type: Sequelize.STRING,
-		unique: true,
-	},
-	username: Sequelize.STRING,
-	usage_count: {
-		type: Sequelize.INTEGER,
-		defaultValue: 0,
-		allowNull: false,
-	},
-});
+initializeCoinsDatabase();
 
-bot.once('ready', () => {
-    inventario.sync();
-});
-
-/*
-bot.on('message', async message => {
-	if (message.content.startsWith(config.prefix)) {
-		const input = message.content.slice(config.prefix.length).split(' ');
-		const command = input.shift();
-		const commandArgs = input.join(' ');
-
-		if (command === 'addtag') {
-			// [delta]
-		} else if (command === 'tag') {
-			// [epsilon]
-		} else if (command === 'edittag') {
-			// [zeta]
-		} else if (command === 'taginfo') {
-			// [theta]
-		} else if (command === 'showtags') {
-			// [lambda]
-		} else if (command === 'removetag') {
-			// [mu]
-		}
-	}
-});
-*/
-
-////////////////////////////////////////////////////
-
-bot.on('message', (message) => {
-    if(message.content.toLowerCase().includes('juanma')){
-        message.channel.send ('altogato');
-    }
-})
+juanmaFeature();
 
 bot.login(config.token); 
+
+
+//FUNCIONES:
+
+function initializeBot(){
+	const bot = new commando.Client({
+		commandPrefix:(config.prefix),
+		owner: (config.ownerID),
+	});
+
+	bot.registry.registerDefaultTypes()
+		.registerGroups([
+			['tradeo', 'Trade']
+		])
+		.registerDefaultGroups()
+		.registerDefaultCommands()
+		.registerCommandsIn(path.join(__dirname, 'commands'));
+
+	bot.once('ready', () => {
+		 console.log('Logged in!');
+		 bot.user.setActivity('kevlar');
+	});
+	return bot
+}
+
+function initializeCoinsDatabase(){
+	
+}
+
+function juanmaFeature(){
+	bot.on('message', (message) => {
+		if(message.content.toLowerCase().includes('juanma')){
+			message.channel.send ('altogato');
+		}
+	});
+}
